ca0db41ed0c70001f8560ff4c7eb9759d4b6d15c
elthorne <46898565+elthorne@users.noreply.github.com>
2023-12-18 10:32:31 +0000

    debug: Add console.log to determine route of error
    refs: BLAIS5-3756

--- src/Functions.ts
+++ src/Functions.ts
@@ -40,6 +40,7 @@ export function dd_filename_to_data(dd_filename: string): DataDeliveryFile {
 
 export function batch_to_data(batchName: string): DataDeliveryBatchData {
     let [survey, originalDateString, timeString] = ["", "", ""];
+    console.log(`EL'S DEBUG: Type of myVariable is ${typeof batchName}`);
 
     if (batchName.match(/^[0-9]{8}_[0-9]{6}$/)) {
         // example 26032021_080842
